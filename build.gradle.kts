ae8117369e55edffc47f99fd19bf29591c69a160
Alexander Gorbunov <alexander.gorbunov@webware-kassel.de>
2024-09-30 06:07:15 +0200

    basic styles and added java llama inference

--- build.gradle.kts
+++ build.gradle.kts
@@ -47,6 +47,9 @@ dependencies {
         zipSigner()
         testFramework(TestFrameworkType.Platform)
     }
+
+    // Java Ollama C/C++ bindings
+    implementation("de.kherud:llama:3.4.1")
 }
 
 // Configure IntelliJ Platform Gradle Plugin - read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-extension.html
--- src/main/java/com/github/gorbunovalex/caug/toolWindow/CAugToolWindowFactory.java
+++ src/main/java/com/github/gorbunovalex/caug/toolWindow/CAugToolWindowFactory.java
@@ -1,29 +1,29 @@
 package com.github.gorbunovalex.caug.toolWindow;
 
-import com.intellij.openapi.components.Service;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowFactory;
+import com.intellij.ui.components.JBBox;
 import com.intellij.ui.components.JBLabel;
 import com.intellij.ui.components.JBPanel;
+import com.intellij.ui.components.JBTextField;
 import com.intellij.ui.content.ContentFactory;
-import com.github.gorbunovalex.caug.MyBundle;
 import com.github.gorbunovalex.caug.services.MyProjectService;
 import javax.swing.JButton;
-import javax.swing.event.ChangeListener;
+import java.awt.*;
 
-public class MyToolWindowFactory implements ToolWindowFactory {
+public class CAugToolWindowFactory implements ToolWindowFactory {
 
-    private static final Logger LOGGER = Logger.getInstance(MyToolWindowFactory.class);
+    private static final Logger LOGGER = Logger.getInstance(CAugToolWindowFactory.class);
 
-    public MyToolWindowFactory() {
+    public CAugToolWindowFactory() {
         LOGGER.warn("Don't forget to remove all non-needed sample code files with their corresponding registration entries in `plugin.xml`.");
     }
 
     @Override
     public void createToolWindowContent(Project project, ToolWindow toolWindow) {
-        MyToolWindow myToolWindow = new MyToolWindow(toolWindow);
+        CAugToolWindow myToolWindow = new CAugToolWindow(toolWindow);
         ContentFactory contentFactory = ContentFactory.getInstance();
         contentFactory.createContent(myToolWindow.getContent(), null, false);
         toolWindow.getContentManager().addContent(contentFactory.createContent(myToolWindow.getContent(), null, false));
@@ -34,21 +34,30 @@ public class MyToolWindowFactory implements ToolWindowFactory {
         return true;
     }
 
-    public static class MyToolWindow {
+    public static class CAugToolWindow {
 
         private final MyProjectService service;
 
-        public MyToolWindow(ToolWindow toolWindow) {
+        public CAugToolWindow(ToolWindow toolWindow) {
             this.service = toolWindow.getProject().getService(MyProjectService.class);
         }
 
         public JBPanel getContent() {
-            JBPanel panel = new JBPanel();
-            JBLabel label = new JBLabel("Hello");
-            JButton button = new JButton("World");
-            button.addActionListener(e -> label.setText("ITS ALIVE"));
-            panel.add(label);
-            panel.add(button);
+            LayoutManager layout = new GridLayout(2, 1);
+            JBPanel panel = new JBPanel(layout);
+
+            JBBox inputBox = JBBox.createVerticalBox();
+            JBTextField textField = new JBTextField();
+            JButton button = new JButton("Send");
+            inputBox.add(textField);
+            inputBox.add(button);
+
+            JBBox answerBox = new JBBox(1);
+            answerBox.add(new JBLabel("Answer"));
+
+            panel.add(new JBLabel("Chat with Ollama"));
+            panel.add(inputBox);
+            panel.add(answerBox);
             return panel;
         }
     }
