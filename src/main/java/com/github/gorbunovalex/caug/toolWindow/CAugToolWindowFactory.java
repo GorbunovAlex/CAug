4b0214e867411a228a9dcee80a4136d840add9a2
Alexander Gorbunov <alexander.gorbunov@webware-kassel.de>
2024-10-04 09:10:59 +0200

    updated interface, added service

--- src/main/java/com/github/gorbunovalex/caug/toolWindow/CAugToolWindowFactory.java
+++ src/main/java/com/github/gorbunovalex/caug/toolWindow/CAugToolWindowFactory.java
@@ -1,5 +1,6 @@
 package com.github.gorbunovalex.caug.toolWindow;
 
+import com.github.gorbunovalex.caug.services.LlamaService;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.ToolWindow;
@@ -9,9 +10,11 @@ import com.intellij.ui.components.JBLabel;
 import com.intellij.ui.components.JBPanel;
 import com.intellij.ui.components.JBTextField;
 import com.intellij.ui.content.ContentFactory;
-import com.github.gorbunovalex.caug.services.MyProjectService;
+
 import javax.swing.JButton;
 import java.awt.*;
+import java.beans.EventHandler;
+import java.io.IOException;
 
 public class CAugToolWindowFactory implements ToolWindowFactory {
 
@@ -36,28 +39,40 @@ public class CAugToolWindowFactory implements ToolWindowFactory {
 
     public static class CAugToolWindow {
 
-        private final MyProjectService service;
+        private final LlamaService llama_service;
 
         public CAugToolWindow(ToolWindow toolWindow) {
-            this.service = toolWindow.getProject().getService(MyProjectService.class);
+            this.llama_service = toolWindow.getProject().getService(LlamaService.class);
         }
 
         public JBPanel getContent() {
-            LayoutManager layout = new GridLayout(2, 1);
+            LayoutManager layout = new BorderLayout();
             JBPanel panel = new JBPanel(layout);
 
             JBBox inputBox = JBBox.createVerticalBox();
             JBTextField textField = new JBTextField();
             JButton button = new JButton("Send");
+
+            button.addActionListener(e -> {
+                String text = textField.getText();
+                textField.setText("");
+                System.out.println(text);
+                try {
+                    this.llama_service.main(text);
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
+                }
+            });
             inputBox.add(textField);
             inputBox.add(button);
 
             JBBox answerBox = new JBBox(1);
             answerBox.add(new JBLabel("Answer"));
+            answerBox.setPreferredSize(new Dimension(200, 500));
 
-            panel.add(new JBLabel("Chat with Ollama"));
-            panel.add(inputBox);
-            panel.add(answerBox);
+            panel.add(new JBLabel("Chat with Ollama"), BorderLayout.PAGE_START);
+            panel.add(inputBox, BorderLayout.CENTER);
+            panel.add(answerBox, BorderLayout.PAGE_END);
             return panel;
         }
     }
